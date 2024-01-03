589bc45f7dff7775fc60230ce7e65fd172a6dec1
ghannam omar <ghannam.omar@hotmail.com>
2024-01-03 11:56:58 +0100

    Adding and referencing  a "dbt ephemeral models" -  ephemerals are interpreted as CTE

--- lessons/models/marts/orders.sql
+++ lessons/models/marts/orders.sql
@@ -7,17 +7,13 @@ order_item_measures AS (
 		SUM(item_sale_price) AS total_sale_price,
 		SUM(product_cost) AS total_product_cost,
 		SUM(item_profit) AS total_profit,
-		SUM(item_discount) AS total_discount,
+		SUM(item_discount) AS total_discount
 
-
-	FROM {{ref("int_ecommerce__order_items_products") }}
+	FROM {{ ref('int_ecommerce__order_items_products') }}
 	GROUP BY 1
-
 )
 
 SELECT
-
-	-- Dimensions from our orders table
 	od.order_id,
 	od.created_at AS order_created_at,
 	od.shipped_at AS order_shipped_at,
@@ -25,13 +21,17 @@ SELECT
 	od.returned_at AS order_returned_at,
 	od.status AS order_status,
 	od.num_items_ordered,
-
-	-- Metrics on an orer level
 	om.total_sale_price,
 	om.total_product_cost,
 	om.total_profit,
-	om.total_discount
+	om.total_discount,
 
-FROM {{ref("stg_ecommerce__orders")}} AS od
-LEFT OUTER JOIN order_item_measures AS om
+	-- In practise we'd calculate this column in the model itself, but it's
+	-- a good way to demonstrate how to use an ephemeral materialisation
+	TIMESTAMP_DIFF(od.created_at, user_data.first_order_created_at, DAY) AS days_since_first_order
+
+FROM {{ ref('stg_ecommerce__orders') }} AS od
+LEFT JOIN order_item_measures AS om
 	ON od.order_id = om.order_id
+LEFT JOIN {{ ref('int_ecommerce__first_order_created') }} AS user_data
+	ON od.user_id = user_data.user_id
